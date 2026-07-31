402d0ebc2d97a7346d6666ab4043b8cc63f80f21
Francisco Sanabria <fsanabria@fastmail.com>
2026-07-31 17:07:49 -0600

    uodating tf files

--- 06_Terraform_Basics/06_02_terraform_foundation/project/c1-versions.tf
+++ 06_Terraform_Basics/06_02_terraform_foundation/project/c1-versions.tf
@@ -1,4 +1,20 @@
 # Terraform Block
-
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0" # In production grade, the best recommended version option.
+    }                    # Major versions upgrades are not recommended.
+    random = {
+      source  = "hashicorp/random"
+      version = "3.8.1"
+    }
+  }
+}
 
 # Provider Block
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
--- 06_Terraform_Basics/06_02_terraform_foundation/project/c2-s3bucket.tf
+++ 06_Terraform_Basics/06_02_terraform_foundation/project/c2-s3bucket.tf
@@ -1,1 +1,18 @@
+# Aqui estamos colocando un bloque random_string para generar un nombre aleatorio para el bucket.
+# Esto por que AWS requiere nombres completamente unicos.
+# Y le estamos llamando suffix, simplemente para referenciarlo en el bloque de la variable.
 
+resource "random_string" "suffix" {
+  length = 6
+  special = false
+  upper = false
+}
+
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "devopsdemo-${random_string.suffix.result}"
+
+  tags = {
+    Name        = "DevOps Demo Bucket"
+    Environment = "Dev"
+  }
+}
